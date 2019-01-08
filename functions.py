10fd3977682d70a6173203ee260a9abcadc0499b
koshelev <koshelev@fnal.gov>
2019-01-08 16:42:56 -0600

    Moved refprop stuff to separate module.

--- functions.py
+++ functions.py
@@ -1,164 +1,12 @@
 from math import log, log10
-from pyrefprop import refprop as rp
-from pint import UnitRegistry
-import logging
-import logging.config
-from functools import wraps
-import sys, os
-__location__ = os.path.dirname(os.path.abspath(__file__))
+from heat_transfer.rp_wrapper import *
 
-# load the logging configuration
-logging.config.fileConfig(os.path.join(__location__, 'logging.ini'))
-
-logger = logging.getLogger(__name__)
-
-ureg = UnitRegistry(autoconvert_offset_to_baseunit = True)
+#' Configuring units package:
 Q_ = ureg.Quantity
-ureg.load_definitions(os.path.join(__location__, 'pint definitions.txt'))
-sigma = Q_('stefan_boltzmann_constant')
-
-#Setting units for "standard" flow
-T_NTP = Q_(68, ureg.degF) #Normal Temperature (NIST)
-P_NTP = Q_(14.7, ureg.psi) #Normal Pressure (NIST)
-
-T_MSC = Q_(15, ureg.degC) #Metric Standard Conditions (used by Crane TP-410)
-P_MSC = Q_(101325, ureg.Pa) #Metric Standard Conditions (used by Crane TP-410)
-
-Air = {'fluid':'air', 'P':P_NTP, 'T':T_NTP}
-
-#flsh = ureg.wraps (None, (None, ureg.K, ureg.kPa, None))(rp.flsh)
-def flsh(routine, var1, var2, x, kph=1):
-    """
-    Multifunctional wrapper for refprop flsh function
-    """
-    if routine == 'TP':   #temperature; pressure
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2.to(ureg.kPa).magnitude
-    if routine == 'TD':   #temperature; Molar Density
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2.to(ureg.mol/ureg.L).magnitude
-    if routine == 'TH':   #temperature; enthalpy
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'TS':   #temperature; entropy
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
-    if routine == 'TE':   #temperature; internal energy
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'PD':   #pressure; molar density
-        var1_unitless = var1.to(ureg.kPa).magnitude
-        var2_unitless = var2.to(ureg.mol/ureg.L).magnitude
-    if routine == 'PH':   #pressure; enthalpy
-        var1_unitless = var1.to(ureg.kPa).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'PS':   #pressure; entropy
-        var1_unitless = var1.to(ureg.kPa).magnitude
-        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
-    if routine == 'PE':   #pressure; internal energy
-        var1_unitless = var1.to(ureg.kPa).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'HS':   #enthalpy; entropy
-        var1_unitless = var1.to(ureg.J/ureg.mol).magnitude
-        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
-    if routine == 'ES':   #internal energy; entropy
-        var1_unitless = var1.to(ureg.J/ureg.mol).magnitude
-        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
-    if routine == 'DH':   #molar density; enthalpy
-        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'DS':   #molar density; entropy
-        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
-        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
-    if routine == 'DE':   #molar density; internal energy
-        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
-        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
-    if routine == 'TQ':   #temperature; vapour quality
-        var1_unitless = var1.to(ureg.K).magnitude
-        var2_unitless = var2
-    if routine == 'PQ':   #pressure; vapour qaulity
-        var1_unitless = var1.to(ureg.kPa).magnitude
-        var2_unitless = var2
-
-    refprop_output = rp.flsh(routine, var1_unitless, var2_unitless, x, kph=1)
-    return rp_out_unit(refprop_output)
-
-
-
-
-Outputs = {'t':ureg.K, 'p':ureg.kPa, 'D':ureg.mol/ureg.L, 'Dliq':ureg.mol/ureg.L, 'Dvap':ureg.mol/ureg.L,
-           'x':None, 'xliq':None, 'xvap':None, 'q':None, 'e':ureg.J/ureg.mol, 'h':ureg.J/ureg.mol,
-           's':ureg.J/ureg.mol*ureg.K, 'cv':ureg.J/(ureg.mol*ureg.K), 'cp':ureg.J/(ureg.mol*ureg.K),
-           'w':ureg.m/ureg.s, 'hfmix':None, 'kph':None, 'hrf':None, 'hfld':None, 'nc':None,
-           'xkappa':None, 'beta':None, 'xisenk':None, 'xkt':None, 'betas':None, 'bs':None, 'xkkt':None,
-           'thrott':None, 'pint':None, 'spht':ureg.J/ureg.mol, 'wmm':ureg.g/ureg.mol, 'ttrp':ureg.K,
-           'tnbpt':ureg.K, 'tcrit':ureg.K, 'pcrit':ureg.kPa, 'Dcrit':ureg.mol/ureg.L, 'zcrit':None,
-           'acf':None, 'dip':ureg.debye, 'Rgas':ureg.J/(ureg.mol*ureg.K), 'icomp':None,
-           'dpdD':ureg.kPa*ureg.L/ureg.mol, 'd2pdD2':ureg.kPa*ureg.L**2/ureg.mol**2,
-           'dpdt':ureg.kPa/ureg.K, 'dDdt':ureg.mol/(ureg.L*ureg.K), 'dDdp':ureg.mol/(ureg.L*ureg.kPa),
-           'Z':None, 'hjt':ureg.K/ureg.kPa, 'A':ureg.J/ureg.mol, 'G':ureg.J/ureg.mol, 'hmxnme':None,
-           }
-
-def rp_value(value, name):
-    """Prepare the quantity for passing to rp function by obtaining a dimensionless value.
-    """
-    if Outputs[name]:
-        return value.to(Outputs[name]).magnitude
-    else:
-        return value
-
-def rp_out_unit (rp_output):
-    """ Add units to output of a refprop function.
-    """
-    Output_w_units = {}
-    for quantity, value in rp_output.items():
-        try:
-            unit = Outputs[quantity]
-            if unit:
-                Output_w_units[quantity] = value*unit
-            else:
-                Output_w_units[quantity] = value
-        except KeyError:
-            logger.warning('Quantity is missing from unit list, please update: {}'.format(quantity))
-    return Output_w_units
-
-def rp_unitize(*names):
-    """Decorator for refprop functions.
-    Given the string names of the variables convert input to rp accepted dimensionless values.
-    Results are parsed to assign units.
-    Use:
-    @rp_unitize('p', 'x', 'kph')
-    def satp(p, x, kph=2):
-        return rp.satp(p, x, kph)
-    """
-    def rp_decorate(rp_func):
-        def rp_wrapper(*rp_args, **rp_kwargs):
-            rp_input_values = list(rp_args) + list(rp_kwargs.values())
-            rp_input = []
-            for value,name in zip(rp_input_values,names):
-                rp_input.append(rp_value(value,name))
-            return rp_out_unit(rp_func(*rp_input))
-        return rp_wrapper
-    return rp_decorate
-
-@rp_unitize('p', 'x', 'kph')
-def satp(p, x, kph=2):
-    return rp.satp(p, x, kph)
-
-@rp_unitize('t', 'D', 'x')
-def therm2(t, D, x):
-    return rp.therm2(t, D, x)
-
-@rp_unitize('t', 'D', 'x')
-def therm3(t, D, x):
-    return rp.therm3(t, D, x)
-
-@rp_unitize('icomp')
-def info(icomp=1):
-    return rp.info(icomp)
 
 def latent_heat(Fluid_data):
-    """Calculate latent heat/specific heat input for given conditions.
+    """
+    Calculate latent heat/specific heat input for given conditions.
     """
     x,M,D = rp_init(Fluid_data)
     _,T,P = unpack_fluid(Fluid_data)
@@ -182,11 +30,6 @@ def latent_heat(Fluid_data):
         L = therm3(T,D,x)['spht']/M #Specific heat input
     return L
 
-
-
-trnprp = ureg.wraps (None, (ureg.K, ureg('mol/L'), None))(rp.trnprp)
-
-
 def gamma (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
     """
     Calculate gamma (k) coefficient for the fluid
@@ -200,37 +43,11 @@ def gamma (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.d
     return Cp/Cv
 k = gamma
 
-def pack_fluid (fluid, T_fluid = Q_(15, ureg.degC), P_fluid = Q_(101325, ureg.Pa)):
-        return {'fluid':fluid, 'P':P_fluid, 'T':T_fluid}
-
-def unpack_fluid (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
-        fluid = Fluid_data.get('fluid')
-        T_fluid = Fluid_data.get('T')
-        P_fluid = Fluid_data.get('P')
-        return (fluid, T_fluid, P_fluid)
-
-def rp_init (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
-        """
-        Shortcut initialization of refprop
-        Returns (x, M, D_fluid)
-        """
-        (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
-        prop = rp.setup('def', fluid)
-        x = prop.get('x', [1]) 
-        M = rp.wmol(x)['wmix']*ureg('g/mol')
-        if T_fluid and P_fluid:
-            fluid_prop = flsh ("TP", T_fluid, P_fluid, x)
-            D_fluid = fluid_prop['D'] 
-            return (x, M, D_fluid)
-        else: 
-            return (x, M)
-
-
+#' Functions for basic dimensionless quantities (Re number is a part of piping module).
 def Pr (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
         """
         Calculate Prandtl number for given fluid
         Properties of fluid are obtained from Refprop package
-        Units are handled by natu package
         """
         (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
         (x, M, D_fluid) = rp_init(Fluid_data)
@@ -245,7 +62,6 @@ def Gr (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC
         """
         Calculate Grashof number for given fluid and surface
         Properties of fluid are obtained from Refprop package
-        Units are handled by natu package
         """
         (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
         T_surf = Surface_data['T']
@@ -262,11 +78,12 @@ def Ra (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC
         """
         Calculate Rayleigh number for given fluid and surface
         Properties of fluid are obtained from Refprop package
-        Units are handled by natu package
         """
         return  Gr(Fluid_data, Surface_data)*Pr(Fluid_data) 
 
-def heat_trans_coef (k_fluid = 0.02647*ureg('W/(m*K)'), Nu = 4, Dim = Q_(1.315,ureg.inch), Case = {'convection':'free', 'body':'cyl_hor'}):
+
+#' Nu routine calculates both Nu number and convective heat transfer coefficient.
+def  heat_trans_coef (k_fluid = 0.02647*ureg('W/(m*K)'), Nu = 4, Dim = Q_(1.315,ureg.inch), Case = {'convection':'free', 'body':'cyl_hor'}):
         """
         Calculate heat transfer coefficient for Nu routine
         Cases like external flow and pipe inside a pipe have different equations for Nu number - to be implemented
@@ -281,7 +98,6 @@ def Nu (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC
         Calculate Nusselt number for given Fluid/Surface and specific case.
         Only natural convection currently supported.
         Properties of fluid are obtained from Refprop package
-        Units are handled by natu package
         """
         Prandtl = Pr(Fluid_data)
         Convection = not (Case['convection'] == 'free' or Case['convection'] == 'natural')
@@ -321,12 +137,7 @@ def Nu (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC
         h = heat_trans_coef (k_fluid, Nu, Dim, Case)    
         return {'Nu':Nu, 'h':h}
 
-
-
-
-
-
-
+#' Radiation heat leak through the baffles.
 def rad_hl (eps_cold = 0.55, eps_hot = 0.55, T_hot = 300*ureg.K, T_cold = 77*ureg.K, F1_2 = 1, eps_baffle = 0.02, N_baffles = 5):
         """
         Calculate radiative heat load including reduction due to baffles
@@ -340,48 +151,3 @@ def rad_hl (eps_cold = 0.55, eps_hot = 0.55, T_hot = 300*ureg.K, T_cold = 77*ure
         eta = (1+N_baffles*Eps_mut/Eps_baffle_mut)**(-1)
         q_baffle = eta*q0
         return {'q0':q0.to(ureg.W/ureg.m**2), 'q_baffle':q_baffle.to(ureg.W/ureg.m**2), 'eta':eta}
-
-def tc_304(T):
-    Coefs = [-1.4087, 1.3982, 0.2543, -0.6260, 0.2334, 0.4256, -0.4658, 0.1650, -0.0199]
-    log10_k = 0
-    for ind, coef in enumerate(Coefs):
-        log10_k += log10(T.magnitude)**ind*coef
-    return 10**log10_k*ureg('(W/(m*K))')
-
-def polyval(p,x):
-    """Simple evaluation of a polynomial at a specific value using Horner's method
-    """
-    res = 0
-    for coef in p:
-        res = res*x + coef
-    return res
-
-def therm_exp(T, material='304'):
-    """Linear expansion, (L-L_293)/L_293, dimensionless
-    """
-    LinearExpansion = {'304':((1.7127E-8, -2.0261E-5, 9.2683E-3, -3.9811E-1, -2.9554E2,),(23*ureg.K, -300.04), (ureg('4 K'), ureg('300 K'))),
-     } #Data from NIST website
-    (Coeffs, (T_low, low_val), (T_min, T_max)) = LinearExpansion[material]
-    if T<T_min or T>T_max:
-        raise ValueError('NIST model is only defined for T=({T_min:.3~}..{T_max:.3~} for {material}: {T:.3~}'.format(T_min=T_min, T_max=T_max, material=material, T=T))
-    if T<T_low:
-        res = low_val
-    else:
-        res = polyval(Coeffs, T.to(ureg.K).magnitude)
-    return res*1e-5
-
-    
-
-if __name__ == "__main__":
-        print (Ra().to_base_units())
-        print (gamma())
-        print (rp_init({'fluid':'helium', 'T':Q_(20,ureg.degC), 'P':Q_(101325, ureg.Pa)}))
-        print (rp_init({'fluid':'helium', 'T':Q_(4.2,ureg.K), 'P':Q_(101325, ureg.Pa)}))
-        print (satp(Q_(101325, ureg.Pa), [1])['t'])
-        print ('Decorator test:', satp(Q_(101325, ureg.Pa), [1]))
-        print(tc_304(150*ureg.K))
-        Leak = tc_304(150*ureg.K)*3.14159*0.125*ureg.inch*0.035*ureg.inch/(1*ureg.ft)*300*ureg.K
-        print(Leak)
-        print(Leak.to(ureg.W))
-        print((Leak/(7*ureg('kJ/kg'))).to(ureg.g/ureg.s))
-        print(therm_exp(ureg('4.5 K'))*ureg('20 ft').to(ureg.inch))
---
+++ rp_wrapper.py
@@ -0,0 +1,196 @@
+#' % Utilities for hydraulics calculations
+#' % by Sergey Koshelev
+
+#' Importing math functions, unit handling package, and logging. Refprop is used for material properties.
+from math import log, log10
+from pyrefprop import refprop as rp
+from pint import UnitRegistry
+import logging
+import logging.config
+from functools import wraps
+#' Setting up logging configuration file:
+import sys, os
+__location__ = os.path.dirname(os.path.abspath(__file__))
+
+# load the logging configuration:
+logging.config.fileConfig(os.path.join(__location__, 'logging.ini'))
+
+logger = logging.getLogger(__name__)
+
+#' Configuring units package:
+ureg = UnitRegistry(autoconvert_offset_to_baseunit = True)
+Q_ = ureg.Quantity
+ureg.load_definitions(os.path.join(__location__, 'pint definitions.txt'))
+sigma = Q_('stefan_boltzmann_constant')
+
+#Setting units for "standard" flow
+T_NTP = Q_(68, ureg.degF) #Normal Temperature (NIST)
+P_NTP = Q_(14.7, ureg.psi) #Normal Pressure (NIST)
+
+T_MSC = Q_(15, ureg.degC) #Metric Standard Conditions (used by Crane TP-410)
+P_MSC = Q_(101325, ureg.Pa) #Metric Standard Conditions (used by Crane TP-410)
+
+Air = {'fluid':'air', 'P':P_NTP, 'T':T_NTP}
+
+#' Refprop package uses dimensionless values for its functions. A series of wrapper functions are used to allow for units seamless integration.
+def flsh(routine, var1, var2, x, kph=1):
+    """
+    Multifunctional wrapper for refprop flsh function
+    """
+    if routine == 'TP':   #temperature; pressure
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2.to(ureg.kPa).magnitude
+    if routine == 'TD':   #temperature; Molar Density
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2.to(ureg.mol/ureg.L).magnitude
+    if routine == 'TH':   #temperature; enthalpy
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'TS':   #temperature; entropy
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
+    if routine == 'TE':   #temperature; internal energy
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'PD':   #pressure; molar density
+        var1_unitless = var1.to(ureg.kPa).magnitude
+        var2_unitless = var2.to(ureg.mol/ureg.L).magnitude
+    if routine == 'PH':   #pressure; enthalpy
+        var1_unitless = var1.to(ureg.kPa).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'PS':   #pressure; entropy
+        var1_unitless = var1.to(ureg.kPa).magnitude
+        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
+    if routine == 'PE':   #pressure; internal energy
+        var1_unitless = var1.to(ureg.kPa).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'HS':   #enthalpy; entropy
+        var1_unitless = var1.to(ureg.J/ureg.mol).magnitude
+        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
+    if routine == 'ES':   #internal energy; entropy
+        var1_unitless = var1.to(ureg.J/ureg.mol).magnitude
+        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
+    if routine == 'DH':   #molar density; enthalpy
+        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'DS':   #molar density; entropy
+        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
+        var2_unitless = var2.to(ureg.J/(ureg.mol*ureg.K)).magnitude
+    if routine == 'DE':   #molar density; internal energy
+        var1_unitless = var1.to(ureg.mol/ureg.L).magnitude
+        var2_unitless = var2.to(ureg.J/ureg.mol).magnitude
+    if routine == 'TQ':   #temperature; vapour quality
+        var1_unitless = var1.to(ureg.K).magnitude
+        var2_unitless = var2
+    if routine == 'PQ':   #pressure; vapour qaulity
+        var1_unitless = var1.to(ureg.kPa).magnitude
+        var2_unitless = var2
+
+    refprop_output = rp.flsh(routine, var1_unitless, var2_unitless, x, kph=1)
+    return rp_out_unit(refprop_output)
+
+
+
+
+Outputs = {'t':ureg.K, 'p':ureg.kPa, 'D':ureg.mol/ureg.L, 'Dliq':ureg.mol/ureg.L, 'Dvap':ureg.mol/ureg.L,
+           'x':None, 'xliq':None, 'xvap':None, 'q':None, 'e':ureg.J/ureg.mol, 'h':ureg.J/ureg.mol,
+           's':ureg.J/ureg.mol*ureg.K, 'cv':ureg.J/(ureg.mol*ureg.K), 'cp':ureg.J/(ureg.mol*ureg.K),
+           'w':ureg.m/ureg.s, 'hfmix':None, 'kph':None, 'hrf':None, 'hfld':None, 'nc':None,
+           'xkappa':None, 'beta':None, 'xisenk':None, 'xkt':None, 'betas':None, 'bs':None, 'xkkt':None,
+           'thrott':None, 'pint':None, 'spht':ureg.J/ureg.mol, 'wmm':ureg.g/ureg.mol, 'ttrp':ureg.K,
+           'tnbpt':ureg.K, 'tcrit':ureg.K, 'pcrit':ureg.kPa, 'Dcrit':ureg.mol/ureg.L, 'zcrit':None,
+           'acf':None, 'dip':ureg.debye, 'Rgas':ureg.J/(ureg.mol*ureg.K), 'icomp':None,
+           'dpdD':ureg.kPa*ureg.L/ureg.mol, 'd2pdD2':ureg.kPa*ureg.L**2/ureg.mol**2,
+           'dpdt':ureg.kPa/ureg.K, 'dDdt':ureg.mol/(ureg.L*ureg.K), 'dDdp':ureg.mol/(ureg.L*ureg.kPa),
+           'Z':None, 'hjt':ureg.K/ureg.kPa, 'A':ureg.J/ureg.mol, 'G':ureg.J/ureg.mol, 'hmxnme':None,
+           }
+
+def rp_value(value, name):
+    """
+    Prepare the quantity for passing to rp function by obtaining a dimensionless value.
+    """
+    if Outputs[name]:
+        return value.to(Outputs[name]).magnitude
+    else:
+        return value
+
+def rp_out_unit (rp_output):
+    """
+    Add units to output of a refprop function.
+    """
+    Output_w_units = {}
+    for quantity, value in rp_output.items():
+        try:
+            unit = Outputs[quantity]
+            if unit:
+                Output_w_units[quantity] = value*unit
+            else:
+                Output_w_units[quantity] = value
+        except KeyError:
+            logger.warning('Quantity is missing from unit list, please update: {}'.format(quantity))
+    return Output_w_units
+
+def rp_unitize(*names):
+    """
+    Decorator for refprop functions.
+    Given the string names of the variables convert input to rp accepted dimensionless values.
+    Results are parsed to assign units.
+    Use:
+    @rp_unitize('p', 'x', 'kph')
+    def satp(p, x, kph=2):
+        return rp.satp(p, x, kph)
+    """
+    def rp_decorate(rp_func):
+        def rp_wrapper(*rp_args, **rp_kwargs):
+            rp_input_values = list(rp_args) + list(rp_kwargs.values())
+            rp_input = []
+            for value,name in zip(rp_input_values,names):
+                rp_input.append(rp_value(value,name))
+            return rp_out_unit(rp_func(*rp_input))
+        return rp_wrapper
+    return rp_decorate
+
+@rp_unitize('p', 'x', 'kph')
+def satp(p, x, kph=2):
+    return rp.satp(p, x, kph)
+
+@rp_unitize('t', 'D', 'x')
+def therm2(t, D, x):
+    return rp.therm2(t, D, x)
+
+@rp_unitize('t', 'D', 'x')
+def therm3(t, D, x):
+    return rp.therm3(t, D, x)
+
+@rp_unitize('icomp')
+def info(icomp=1):
+    return rp.info(icomp)
+
+trnprp = ureg.wraps (None, (ureg.K, ureg('mol/L'), None))(rp.trnprp)
+
+#' The package uses unified way of keeping track of fluid state, specifying fluid, pressure, temperature. Helper functions to pack and unpack the fluid state.
+def pack_fluid (fluid, T_fluid = Q_(15, ureg.degC), P_fluid = Q_(101325, ureg.Pa)):
+        return {'fluid':fluid, 'P':P_fluid, 'T':T_fluid}
+
+def unpack_fluid (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
+        fluid = Fluid_data.get('fluid')
+        T_fluid = Fluid_data.get('T')
+        P_fluid = Fluid_data.get('P')
+        return (fluid, T_fluid, P_fluid)
+
+#' To use refprop one needs first to set up the fluid. Following function simplifies initialization and also returns most commonly required properties when possible.
+def rp_init (Fluid_data = {'fluid':'air', 'P':Q_(101325,ureg.Pa), 'T':Q_(15,ureg.degC)}):
+        """
+        Shortcut initialization of refprop
+        Returns (x, M, D_fluid)
+        """
+        (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
+        prop = rp.setup('def', fluid)
+        x = prop.get('x', [1]) 
+        M = rp.wmol(x)['wmix']*ureg('g/mol')
+        if T_fluid and P_fluid:
+            fluid_prop = flsh ("TP", T_fluid, P_fluid, x)
+            D_fluid = fluid_prop['D'] 
+            return (x, M, D_fluid)
+        else: 
+            return (x, M)
