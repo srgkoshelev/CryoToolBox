b653b00738ac6bf7e0ab4cda7c70d17656625e94
koshelev <koshelev@fnal.gov>
2019-03-04 21:29:32 -0600

    Cleaned and fixed calculations. Basic specific heat calculation is now possible. Needs to be moved to functions.

--- cga.py
+++ cga.py
@@ -1,8 +1,6 @@
 #!python3
 from math import *
-from pyrefprop import refprop as rp
 from heat_transfer import functions as ht
-from heat_transfer.piping import *
 ureg = ht.ureg
 Q_ = ureg.Quantity
 
@@ -12,19 +10,20 @@ def max_theta(Fluid_data, step = 0.01):
         SHI - specific heat input v*(dh/dv)|p
         step - temperature step
         """
-        x, __ = ht.rp_init(Fluid_data)
-        T_start = rp.satp(Q_(101325, ureg.Pa),x)['t']*ureg.K #Starting temperature - liquid temperature for atmospheric pressure. Vacuum should be handled separately.
+        x, _, _ = ht.rp_init(Fluid_data)
+        T_start = ht.satp(Q_(101325, ureg.Pa),x)['t']  #Starting temperature - liquid temperature for atmospheric pressure. Vacuum should be handled separately.
         T_end = 300*ureg.K
-        theta = 0
+        theta = ht.ureg('0 mol**1.5/(J*l**0.5)')
         T = T_start
+        P = Fluid_data['P']
         while T <= T_end:
-                D_vap = ht.flsh('TP', T, P, x)['Dvap']
-                theta_new = (D_vap**0.5)/rp.therm3(T, D_vap, x)['spht'] #TODO need wrapper
-                if theta_new > theta:
-                        theta = theta_new
-                else:
-                        break #Function has only one maximum
-                T += step*ureg.K
+            D_vap = ht.flsh('TP', T, P, x)['Dvap']
+            theta_new = (D_vap**0.5)/ht.therm3(T, D_vap, x)['spht'] 
+            if theta_new > theta:
+                theta = theta_new
+            else:
+                break #Function has only one maximum
+            T += step*ureg.K
         return T
 
 
@@ -34,7 +33,7 @@ def max_theta(Fluid_data, step = 0.01):
 #        Based on CGA S-1.3 2008 6.2.2. F = 1.
 #
 #        """
-#        (x, M, D_fluid) = rp_init(Pipe['fluid'])
+#        (x, M, D_fluid) = ht.rp_init(Pipe['fluid'])
 #        P = Pipe['fluid']['P']
 #        P_crit = rp.critp(x)['pcrit']*kPa #Critical pressure 
 #        T = Pipe['fluid']['T']
@@ -88,7 +87,7 @@ def max_theta(Fluid_data, step = 0.01):
 #        Diam = OD(Pipe)
 #        L = Pipe['L']
 #
-#        (x, M, D_fluid) = rp_init(Pipe['fluid'])
+#        (x, M, D_fluid) = ht.rp_init(Pipe['fluid'])
 #        P = Pipe['fluid']['P']
 #        P_crit = rp.critp(x)['pcrit']*kPa #Critical pressure; 
 #        T = Pipe['fluid']['T']
@@ -130,13 +129,13 @@ def to_scfma (M_dot, Fluid_data):
         Convert mass flow rate into SCFM of air.
         Assumption: Flow through a relief device with invariant Area/discharge coefficient (KA).
         """
-        (x_fluid, M_fluid, D_fluid) = rp_init(Fluid_data)
+        (x_fluid, M_fluid, D_fluid) = ht.rp_init(Fluid_data)
         T_fluid = Fluid_data['T']
-        Z_fluid = therm2(T_fluid, D_fluid, x_fluid)['Z'] #Compressibility factor
-        (x_air, M_air, D_air) = rp_init(Air)
-        T_air = Air['T']
-        Z_air = therm2(T_air, D_air, x_air)['Z'] #Compressibility factor
-        Q_air =  M_dot*C_gas_const(Air)/(D_air*M_air*C_gas_const(Fluid_data))*(T_fluid*Z_fluid*M_air/(M_fluid*T_air*Z_air))**0.5
+        Z_fluid = ht.therm2(T_fluid, D_fluid, x_fluid)['Z'] #Compressibility factor
+        (x_air, M_air, D_air) = ht.rp_init(ht.Air)
+        T_air = ht.Air['T']
+        Z_air = ht.therm2(T_air, D_air, x_air)['Z'] #Compressibility factor
+        Q_air =  M_dot*C_gas_const(ht.Air)/(D_air*M_air*C_gas_const(Fluid_data))*(T_fluid*Z_fluid*M_air/(M_fluid*T_air*Z_air))**0.5
         Q_air.ito(ureg.ft**3/ureg.min)
         return Q_air
 
@@ -144,13 +143,13 @@ def from_scfma (Q_air, Fluid_data):
         """
         Convert SCFM of air into mass flow of specified fluid.
         """
-        (x_fluid, M_fluid, D_fluid) = rp_init(Fluid_data)
+        (x_fluid, M_fluid, D_fluid) = ht.rp_init(Fluid_data)
         T_fluid = Fluid_data['T']
-        Z_fluid = therm2(T_fluid, D_fluid, x_fluid)['Z'] #Compressibility factor
-        (x_air, M_air, D_air) = rp_init(Air)
-        T_air = Air['T']
-        Z_air = therm2(T_air, D_air, x_air)['Z'] #Compressibility factor
-        M_dot =  Q_air/(C_gas_const(Air))*(D_air*M_air*C_gas_const(Fluid_data))/(T_fluid*Z_fluid*M_air/(M_fluid*T_air*Z_air))**0.5
+        Z_fluid = ht.therm2(T_fluid, D_fluid, x_fluid)['Z'] #Compressibility factor
+        (x_air, M_air, D_air) = ht.rp_init(ht.Air)
+        T_air = ht.Air['T']
+        Z_air = ht.therm2(T_air, D_air, x_air)['Z'] #Compressibility factor
+        M_dot =  Q_air/(C_gas_const(ht.Air))*(D_air*M_air*C_gas_const(Fluid_data))/(T_fluid*Z_fluid*M_air/(M_fluid*T_air*Z_air))**0.5
         M_dot.ito(ureg.kg/ureg.s)
         return M_dot
 
@@ -163,22 +162,37 @@ def C_gas_const (Fluid_data):
         C = 520*(k*(2/(k+1))**((k+1)/(k-1)))**0.5*ureg('lb/(hr*lbf)*(degR)^0.5')
         return C
 
-#def make_sections(Piping, Section_start): #TODO Where is this used? Do I need it still?
-#        """
-#        Separate Piping into independent sections for relief capacity calculation
-#        """
-#        Sections = []
-#        for i, ind in enumerate(Section_start):
-#                if i == 0:
-#                        start = 0
-#                end = ind
-#                Sections.append(Piping[start:end])
-#                start = ind
-#        Sections.append(Piping[start:])
-#        return Sections
+def is_super_crit(Fluid_data):
+    x, M, D_fluid = ht.rp_init(Fluid_data)
+    P_crit = ht.critp(x)['pcrit']
+    P = Fluid_data['P']
+    return P > P_crit
 
+def spec_heat(Fluid_data):
+    """
+    Calculate specific heat for given Fluid State.
+    For subcritical flow returns latent heat of evaporation,
+    for supercritical: specific heat input
+    """
+    x, M, D_fluid = ht.rp_init(Fluid_data)
+    _, T, P = ht.unpack_fluid(Fluid_data)
+    Z = ht.therm2(T, D_fluid, x)['Z'] #Compressibility factor
+    C = C_gas_const(Fluid_data)
+    if is_super_crit(Fluid_data):
+        theta = ht.therm3(T, D_fluid, x)['spht']/M
+        spec_heat = theta #For simplification
+    else:
+        h_vapor = ht.flsh("PQ",P,1,x)['h']/M #Saturated vapor enthalpy
+        h_liquid = ht.flsh("PQ",P,0,x)['h']/M #Saturated liquid enthalpy
+        r = h_vapor - h_liquid
+        spec_heat = r #For simplification
+    return spec_heat
 
 
 if __name__ == "__main__":
     print ("100 SCFM of air is equivalent to {:.3g} of Nitrogen flow for P = 1 atm and T = 38 C.".format(from_scfma(100*ureg('ft^3/min'), {'fluid':'air', 'P':1*ureg.atm, 'T':38*ureg.degC})))
     print ("CGA S-1.3 Formula from 6.1.4 a) gives 0.0547 kg/s for the same air capacity.")
+    Fluid = ht.Air
+    Fluid['P'] = ht.ureg('690 kPa')
+    print(max_theta(Fluid))
+    print(spec_heat(Fluid))
