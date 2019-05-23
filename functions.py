471bc3cb5670c230a666c9a442672315bd5ae631
koshelev <koshelev@fnal.gov>
2019-05-23 14:10:04 -0500

    Added and refactored Nu/heat_transfer_coef routine.

--- functions.py
+++ functions.py
@@ -119,3 +119,97 @@ def rad_hl (eps_cold = 0.55, eps_hot = 0.55, T_hot = 300*ureg.K, T_cold = 77*ure
         eta = (1+N_baffles*Eps_mut/Eps_baffle_mut)**(-1)
         q_baffle = eta*q0
         return {'q0':q0.to(ureg.W/ureg.m**2), 'q_baffle':q_baffle.to(ureg.W/ureg.m**2), 'eta':eta}
+
+#' Functions for basic dimensionless quantities (Re number is a part of piping module).
+def Pr(Fluid_data):
+        """
+        Calculate Prandtl number for given fluid
+        Properties of fluid are obtained from Refprop package
+        """
+        (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
+        (x, M, D_fluid) = rp_init(Fluid_data)
+        fluid_trans_prop = trnprp(T_fluid, D_fluid, x)
+        mu = fluid_trans_prop['eta'] #dynamic viscosity
+        k = fluid_trans_prop['tcx'] #thermal conductivity
+        nu = mu/(D_fluid*M) #kinematic viscosity
+        Cp = flsh("TP", T_fluid, P_fluid, x)['cp']/M
+        Prandtl = Cp*mu/k
+        return Prandtl.to_base_units()
+
+def Gr(Fluid_data, T_surf, L_surf):
+        """
+        Calculate Grashof number for given fluid and surface
+        Properties of fluid are obtained from Refprop package
+        L_surf - characteristic length
+        """
+        (fluid, T_fluid, P_fluid) = unpack_fluid(Fluid_data)
+        (x, M, D_fluid) = rp_init(Fluid_data)
+        fluid_trans_prop = trnprp(T_fluid, D_fluid, x)
+        mu_fluid = fluid_trans_prop['eta'] #dynamic viscosity
+        nu_fluid = mu_fluid/(D_fluid*M) #kinematic viscosity
+        beta_exp = 1/(T_fluid.to('K')) #volumetric thermal expansion coefficient
+        Grashof = ureg.g_0 * L_surf**3 * beta_exp * (T_fluid-T_surf) / nu_fluid**2 #Grashof number
+        return Grashof.to_base_units()
+
+def Ra (Fluid_data, T_surf, L_surf):
+        """
+        Calculate Rayleigh number for given fluid and surface
+        Properties of fluid are obtained from Refprop package
+        L_surf - characteristic length
+        """
+        return  Gr(Fluid_data, T_surf, L_surf)*Pr(Fluid_data) 
+
+def Nu_cyl_hor(Fluid_data, T_cyl, D_cyl):
+        """
+        Calculate Nusselt number for given Fluid around horizontal cylinder.
+        Only natural convection currently supported.
+        Properties of fluid are obtained from Refprop package
+        """
+        Prandtl = Pr(Fluid_data)
+        Rayleigh = Ra(Fluid_data, T_cyl, D_cyl)
+        C_l = 0.671/(1+(0.492/Prandtl)**(9/16))**(4/9) #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.13)
+        Nu_T = 0.772*C_l*Rayleigh**(1/4) #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.45)
+        f = 1-0.13/Nu_T**0.16
+        Nu_l = 2*f/log(1+2*f*Nu_T)
+        C_t = 0.0002*log(Prandtl)**3 - 0.0027*log(Prandtl)**2 + 0.0061*log(Prandtl) + 0.1054
+        Nu_t = 0.103*Rayleigh**(1/3)
+        Nusselt = (Nu_l**10 + Nu_t**10)**(1/10) #Nu number, Handbook of heat transfer, Rohsenow, Hartnet, Cho
+        return Nusselt.to_base_units()
+
+def Nu_cyl_vert(Fluid_data, T_cyl, D_cyl, L_cyl):
+        """
+        Calculate Nusselt number for given Fluid around horizontal cylinder.
+        Only natural convection currently supported.
+        Properties of fluid are obtained from Refprop package
+        """
+        Prandtl = Pr(Fluid_data)
+        Rayleigh = Ra(Fluid_data, T_cyl, D_cyl)
+        C_l = 0.671/(1+(0.492/Prandtl)**(9/16))**(4/9) #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.13)
+        C_t_vert = (0.13*Prandtl**0.22)/(1+0.61*Prandtl**0.81)**0.42 #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.24)
+        Nu_T_plate = C_l * Rayleigh**0.25
+        Nu_l_plate = 2 / log(1+2/Nu_T_plate) #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.33)
+        zeta = 1.8 * L_cyl / (D_cyl*Nu_T_plate)  #Handbook of heat transfer, Rohsenow, Hartnet, Cho (4.44)
+        Nu_l = zeta / (log(1+zeta)*Nu_l_plate)
+        Nu_t = C_t_vert*Rayleigh**(1/3)/(1+1.4e9*Prandtl/Rayleigh)
+        Nusselt = (Nu_l**6 + Nu_t**6)**(1/6)
+        return Nusselt.to_base_units()
+
+def  heat_trans_coef(Fluid_data, L_surf, Nu):
+        """
+        Calculate heat transfer coefficient
+        L_surf - characteristic length:
+            Horizontal cylinder: L_surf = D_cyl
+            Vertical cylinder: L_surf = L_cyl
+        """
+        x,M,D = rp_init(Fluid_data)
+        k_fluid = trnprp(T_fluid, D_fluid, x)['tcx']
+        return k_fluid*Nu/L_surf
+
+
+
+
+
+
+
+
+
