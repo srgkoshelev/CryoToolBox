3a5d531db84c76717930b3db9ab131dfd992b695
Sergey Koshelev <koshelev@fnal.gov>
2020-04-26 15:51:45 -0500

    Linted the code.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-""" `heat_transfer` is a Python module for simple heat transfer and hydraulics calculation using [CoolProp](https://github.com/CoolProp/CoolProp) and [Pint](https://github.com/hgrecco/pint) for unit handling.
+"""
+ `heat_transfer` is a Python module for simple heat transfer and hydraulics
+calculation using [CoolProp](https://github.com/CoolProp/CoolProp) and
+[Pint](https://github.com/hgrecco/pint) for unit handling.
 
 Provides:
     1. Thermal properties via open source HEOS or REFPROP back-end
@@ -11,30 +14,30 @@ Provides:
 from pint import UnitRegistry
 import logging
 import logging.config
-import sys, os
+import os
 
-#Setting up logging
+# Setting up logging
 __location__ = os.path.dirname(os.path.abspath(__file__))
 logging.config.fileConfig(os.path.join(__location__, 'logging.ini'))
 logger = logging.getLogger(__name__)
 
-#' Configuring units package:
-ureg = UnitRegistry(autoconvert_offset_to_baseunit = True)
+# Configuring units package:
+ureg = UnitRegistry(autoconvert_offset_to_baseunit=True)
 Q_ = ureg.Quantity
 ureg.load_definitions(os.path.join(__location__, 'pint definitions.txt'))
 
+# Setting units for "standard" flow
+T_NTP = Q_(68, ureg.degF)  # Normal Temperature (NIST)
+P_NTP = Q_(14.7, ureg.psi)  # Normal Pressure (NIST)
 
-#Setting units for "standard" flow
-T_NTP = Q_(68, ureg.degF) #Normal Temperature (NIST)
-P_NTP = Q_(14.7, ureg.psi) #Normal Pressure (NIST)
+T_MSC = Q_(15, ureg.degC)  # Metric Standard Conditions (used by Crane TP-410)
+P_MSC = Q_(101325, ureg.Pa)  # Metric Standard Conditions (used by Crane TP-410)
 
-T_MSC = Q_(15, ureg.degC) #Metric Standard Conditions (used by Crane TP-410)
-P_MSC = Q_(101325, ureg.Pa) #Metric Standard Conditions (used by Crane TP-410)
-
-from .cp_wrapper import *
-#Default fluids
+from .cp_wrapper import ThermState
+# Default fluids
 Air = ThermState('air')
 Air.update('T', T_NTP, 'P', P_NTP)
+
+
 from .functions import *
 from . import piping
-
