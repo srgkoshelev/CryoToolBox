87a67770ca4a15916dcb7dda3051b8f947e8d533
Sergey Koshelev <srg.koshelev@pm.me>
2021-06-08 09:22:21 -0500

    Typo fix.

--- piping.py
+++ piping.py
@@ -950,7 +950,7 @@ class Piping(list):
         '''
         P_0 = self.fluid.P
         if P_0 <= P_out:
-            logger.warning('Input pressure less or equal to output: \
+            logger.warning(f'Input pressure less or equal to output: \
             {P_0:.3g}, {P_out:.3g}')
             return Q_('0 g/s')
         def to_solve(m_dot_gs, P_in_Pa, P_out_Pa):
