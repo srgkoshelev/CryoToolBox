e2b4335829cf676c1880aadd6f974159eafbbb63
Sergey Koshelev <srg.koshelev@proton.me>
2026-05-08 09:40:24 -0700

    Standard conditions won't throw offset error now

--- CryoToolBox/std_conditions.py
+++ CryoToolBox/std_conditions.py
@@ -4,18 +4,18 @@ from importlib.resources import files
 from pint import UnitRegistry
 
 # Configuring units package:
-ureg = UnitRegistry()
+ureg = UnitRegistry(autoconvert_offset_to_baseunit = True)
 Q_ = ureg.Quantity
 __location__ = str(files(__package__))
 ureg.load_definitions(str(files(__package__).joinpath('pint definitions.txt')))
 
 
 # Setting units for "standard" flow
-T_NTP = Q_(68, ureg.degF)  # Normal Temperature (NIST)
-P_NTP = Q_(14.696, ureg.psi)  # Normal Pressure (NIST)
+T_NTP = Q_(68, ureg.degF).to_base_units()  # Normal Temperature (NIST)
+P_NTP = Q_(14.696, ureg.psi).to_base_units()  # Normal Pressure (NIST)
 
-T_MSC = Q_(15, ureg.degC)  # Metric Standard Conditions (Crane TP-410)
-P_MSC = Q_(101325, ureg.Pa)  # Metric Standard Conditions (Crane TP-410)
+T_MSC = Q_(15, ureg.degC).to_base_units()  # Metric Standard Conditions (Crane TP-410)
+P_MSC = Q_(101325, ureg.Pa).to_base_units()  # Metric Standard Conditions (Crane TP-410)
 
-T_STD = Q_(60, ureg.degF)  # Standard conditions (BPVC VIII UG-129 (c))
-P_STD = Q_(14.7, ureg.psi)  # Standard conditions (BPVC VIII UG-129 (c))
+T_STD = Q_(60, ureg.degF).to_base_units()  # Standard conditions (BPVC VIII UG-129 (c))
+P_STD = Q_(14.7, ureg.psi).to_base_units()  # Standard conditions (BPVC VIII UG-129 (c))
